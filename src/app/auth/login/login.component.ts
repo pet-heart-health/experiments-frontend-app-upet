362d03c701534f50872a018a17871d0e95c633a9
Diego Flores <u20221b394@upc.edu.pe>
2025-05-11 21:49:37 -0500

    feat(catpcha): added captcha to login

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import { AuthService } from '../../core/auth/services/auth.service';
 import { catchError, of } from 'rxjs';
@@ -16,14 +16,49 @@ import { navigateTo } from '../shared/auth.utils';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'] // Corrige 'styleUrl' a 'styleUrls'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
   email: string = '';
   password: string = '';
   errorMessage: string | null = null;
 
+  captchaQuestion: string = '';
+  captchaAnswer: string = '';
+  expectedAnswer: number = 0;
+  captchaError: string | null = null;
+
   constructor(private authService: AuthService, private router: Router) {}
 
+  ngOnInit() {
+    this.generateCaptcha();
+  }
+
+  generateCaptcha() {
+    const num1 = Math.floor(Math.random() * 10);
+    const num2 = Math.floor(Math.random() * 10);
+    this.captchaQuestion = `¿Cuánto es ${num1} + ${num2}?`;
+    this.expectedAnswer = num1 + num2;
+    this.captchaAnswer = '';
+    //this.captchaError = null;
+  }
+
+  validateCaptcha(): boolean {
+    const userAnswer = parseInt(this.captchaAnswer);
+    if (isNaN(userAnswer) || userAnswer !== this.expectedAnswer) {
+      this.captchaError = 'Respuesta incorrecta, intenta nuevamente';
+      console.log('error captcha', this.captchaError);
+      this.generateCaptcha();
+      return false;
+    }
+    this.captchaError = null;
+    return true;
+  }
+
   login() {
+
+    if (!this.validateCaptcha()) {
+      return;
+    }
+
     const request: LoginRequest = { email: this.email, password: this.password };
     this.authService.login(request).pipe(
       catchError((error) => {

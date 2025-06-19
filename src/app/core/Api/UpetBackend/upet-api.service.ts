7a23d527ea36b712f8677d4acaaeff332850a9d4
Marcelo rentería <u202216558@upc.edu.pe>
2025-06-19 13:08:30 -0500

    feat: Db fix.

--- src/app/core/Api/UpetBackend/upet-api.service.ts
+++ src/app/core/Api/UpetBackend/upet-api.service.ts
@@ -7,7 +7,7 @@ import { Observable, throwError } from 'rxjs';
 })
 export class UpetApiService {
 
-  private readonly baseUrl: string = 'https://web-production-4270c.up.railway.app';
+  private readonly baseUrl: string = 'https://web-production-4270c.up.railway.app/api/v1';
 
   constructor(protected  http: HttpClient) {}
 
